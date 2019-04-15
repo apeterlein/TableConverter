02c4a00f0dae8384c6d7be7f7954a7ab9b9c76c9
Adam Peterlein <apeterlein@gmail.com>
2019-04-15 11:35:09 -0400

    Add project files.

---
+++ TableConverter/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Linq;
+
+namespace TableConverter
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("GETTING CLIPBOARD");
+                string input = Clipboard.GetText();
+                Console.WriteLine("PARSING INPUT");
+                string output = "\\begin{table}\n\\centering\n\\begin{tabular}{ ";
+                if (input.Contains('\r'))
+                {
+                    input = input.Replace("\r", "");
+                }
+                var rows = input.Split('\n').ToList().Reverse<string>().Skip(1).Reverse<string>();
+                var cells = rows.ToList().Select(x => x.Split('\t').ToList());
+                Console.WriteLine("CONSTRUCTING OUTPUT");
+                output += string.Concat(Enumerable.Repeat("r ", cells.First().Count())) + "}\n";
+                output += string.Join("\\\\\n", cells.Select(x => string.Join("&", x.ToArray())).ToArray());
+                output += "\\\\\n\\end{tabular}\n\\caption{My table}\n\\label{tab:table}\n\\end{table}\n";
+                Console.WriteLine("SETTING CLIPBOARD");
+                Clipboard.SetText(output);
+                Console.WriteLine("DONE PRESS ANY KEY TO EXIT");
+                Console.ReadKey();
+            }
+            catch
+            {
+                Console.WriteLine("ERROR PRESS ANY KEY TO EXIT");
+                Clipboard.SetText("ERROR");
+                Console.ReadKey();
+            }
+        }
+    }
+}
